51d73d4670dba441538d918cd2de10e55de6e5ad
Hannah Fernandes-Martin <hfm11295@gmail.com>
2021-05-07 13:47:56 -0400

    deploying

--- server.js
+++ server.js
@@ -9,7 +9,13 @@ app.use(express.json());
 app.use(express.static("public"));
 app.use(require("./routes/api"));
 app.use(require("./routes/homeRoutes"));
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", { useNewUrlParser: true });
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout",
+    {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        useCreateIndex: true,
+        useFindAndModify: false
+    });
 
 
 app.listen(PORT, () => {
